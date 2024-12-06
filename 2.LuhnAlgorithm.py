f4725f64def30dbb36693af569a0ee37e13b16bd
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-12-06 11:30:09 +0000

    Update 2.LuhnAlgorithm.py

--- 2.LuhnAlgorithm.py
+++ 2.LuhnAlgorithm.py
@@ -5,7 +5,7 @@ usually the last digit of a credit card number. It is a checksum formula that ca
 used to validate various identification numbers, such as credit card numbers, National Provider Identifier (NPI) numbers, Canadian Social Insurance Numbers (SINs), etc.
 '''
 
-def verify_card_number(card_number):
+""" def verify_card_number(card_number):
     '''
     Verifies a credit card number using the Luhn Algorithm.
     
@@ -45,4 +45,42 @@ def main():
 
     verify_card_number(translated_card_number)
 
+main() """
+
+def verify_card_number(card_number):
+    # Reverse the card number to process from the rightmost digit
+    card_number_reversed = card_number[::-1]
+    
+    # Sum the odd-positioned digits
+    sum_of_odd_digits = 0
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+
+    # Sum the even-positioned digits after multiplying them by 2
+    # and subtracting 9 if the result is 10 or greater
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+
+    # Check whether the total sum is divisible by 10
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
+
 main()
+
