8b1037ebe4c0a180336975e9f7945195ca85c4d6
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-12-11 17:03:01 +0000

    Create 4.ListComprehension.py

---
+++ 4.ListComprehension.py
@@ -0,0 +1,11 @@
+'''
+I will learn how to construct a new Python list from an iterable types: lists, tuples, and strings, using List Comprehension.
+This allows me to create a new list without using a for loop or the `.append()` list method.
+
+In this project, I will write a program that takes a string formatted in Camel Case or Pascal Case, then converts it into Snake Case.
+
+The project has two phases: first, I will use a for loop to implement the program.
+Then, I will learn how to use List Comprehension instead of a loop to achieve the same results.
+
+Earlier versions of the code are left commented for reference.
+'''
