d1cd76cf7de2fe18220f2b6fd2f608080baa2a81
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-12-12 18:00:54 +0000

    Create 5.BisectionMethod_SquareRoots.py

---
+++ 5.BisectionMethod_SquareRoots.py
@@ -0,0 +1,10 @@
+'''
+As per the freeCodeCamp.org course on Python:
+
+I will be using numerical methods to approximate solutions to mathematical problems that
+are difficult or impossible to solve analytically.
+
+In this project, I will explore the numerical method of bisection to find the square root
+of a number by iteratively narrowing down the possible range of values that contain
+the square root.
+'''
